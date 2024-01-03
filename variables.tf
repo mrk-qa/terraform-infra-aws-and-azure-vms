d98bd3d62e577159a9e60da5fd5d95832b33bac9
Marco Antonio <marco.ant.seg@gmail.com>
2024-01-03 02:32:00 -0300

    fix: location

--- variables.tf
+++ variables.tf
@@ -11,5 +11,5 @@ variable "azure_key_pub" {
 variable "location" {
   description = "Região onde os recursos serão criados"
   type        = string
-  default     = "East US 2"
+  default     = "East US"
 }

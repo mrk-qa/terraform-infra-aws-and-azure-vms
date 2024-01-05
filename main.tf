c4f25774864cdae693f69d5f63830e99393d61cd
Marco Antonio <marco.ant.seg@gmail.com>
2024-01-05 11:04:35 -0300

    fix: provider azurerm

--- main.tf
+++ main.tf
@@ -32,11 +32,7 @@ provider "aws" {
 }
 
 provider "azurerm" {
-  features {
-    resource_group {
-      prevent_deletion_if_contains_resources = false
-    }
-  }
+  features {}
 }
 
 data "terraform_remote_state" "vpc" {

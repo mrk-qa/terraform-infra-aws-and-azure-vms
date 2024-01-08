11f2474763040b32c3331ffeee116555a64c2f49
Marco Antonio <marco.ant.seg@gmail.com>
2024-01-08 14:08:25 -0300

    add ref var

--- vm-aws.tf
+++ vm-aws.tf
@@ -1,19 +1,21 @@
 resource "aws_key_pair" "key" {
-  key_name   = "aws-key-github"
+  key_name   = var.key_pair_name_aws
   public_key = var.aws_key_pub
 }
 
 resource "aws_instance" "vm" {
-  ami                         = "ami-0c7217cdde317cfec"
-  instance_type               = "t2.micro"
+  ami                         = var.ami_aws
+  instance_type               = var.instance_type_aws
   key_name                    = aws_key_pair.key.key_name
   subnet_id                   = data.terraform_remote_state.vpc.outputs.subnet_id_aws_full
   vpc_security_group_ids      = [data.terraform_remote_state.vpc.outputs.security_group_id_aws_full]
   associate_public_ip_address = true
 
+  availability_zone = var.location_aws
+
   user_data = file("./scripts/index_aws.sh")
 
   tags = {
-    Name = "vm-terraform-github"
+    Name = var.tag_id_aws
   }
 }
--- vm-azure.tf
+++ vm-azure.tf
@@ -1,30 +1,30 @@
 resource "azurerm_resource_group" "resource_group" {
-  name     = "rg-vm-tf-github"
-  location = var.location
+  name     = var.rg_name_azure
+  location = var.location_azure
 
   tags = local.common_tags
 }
 
 resource "azurerm_public_ip" "public_ip" {
-  depends_on = [ azurerm_resource_group.resource_group ]
+  depends_on = [azurerm_resource_group.resource_group]
 
-  name                = "public-ip-terraform-github"
+  name                = var.public_ip_name_azure
   resource_group_name = azurerm_resource_group.resource_group.name
-  location            = var.location
+  location            = var.location_azure
   allocation_method   = "Dynamic"
 
   tags = local.common_tags
 }
 
 resource "azurerm_network_interface" "network_interface" {
-  depends_on = [ azurerm_resource_group.resource_group ]
+  depends_on = [azurerm_resource_group.resource_group]
 
-  name                = "nic-tf-github"
-  location            = var.location
+  name                = var.network_interface_name_azure
+  location            = var.location_azure
   resource_group_name = azurerm_resource_group.resource_group.name
 
   ip_configuration {
-    name                          = "public-ip-terraform-github"
+    name                          = var.ip_configuration_name_azure
     subnet_id                     = data.terraform_remote_state.vnet.outputs.subnet_id_azure_full
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.public_ip.id
@@ -34,8 +34,8 @@ resource "azurerm_network_interface" "network_interface" {
 }
 
 resource "azurerm_network_interface_security_group_association" "association_interface" {
-  depends_on = [ azurerm_resource_group.resource_group ]
-  
+  depends_on = [azurerm_resource_group.resource_group]
+
   network_interface_id      = azurerm_network_interface.network_interface.id
   network_security_group_id = data.terraform_remote_state.vnet.outputs.security_group_id_azure_full
 }
@@ -47,11 +47,11 @@ resource "azurerm_linux_virtual_machine" "vm" {
     azurerm_network_interface_security_group_association.association_interface
   ]
 
-  name                = "vm-terraform-github"
+  name                = var.vm_name_azure
   resource_group_name = azurerm_resource_group.resource_group.name
-  location            = var.location
-  size                = "Standard_B1s"
-  admin_username      = "terraform"
+  location            = var.location_azure
+  size                = var.vm_size_azure
+  admin_username      = var.vm_admin_username_azure
   network_interface_ids = [
     azurerm_network_interface.network_interface.id,
   ]
@@ -59,20 +59,20 @@ resource "azurerm_linux_virtual_machine" "vm" {
   custom_data = base64encode(file("./scripts/index_azure.sh"))
 
   admin_ssh_key {
-    username   = "terraform"
+    username   = var.vm_admin_username_azure
     public_key = var.azure_key_pub
   }
 
   os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
+    caching              = var.vm_os_disk_caching_azure
+    storage_account_type = var.vm_os_disk_storage_azure
   }
 
   source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
-    version   = "latest"
+    publisher = var.vm_ami_publisher_azure
+    offer     = var.vm_ami_offer_azure
+    sku       = var.vm_ami_sku_azure
+    version   = var.vm_ami_version_azure
   }
 
   tags = local.common_tags
